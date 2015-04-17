db09bfada62648990c59d44430d777cd46b5dda6
Ordon <ordon1313@gmail.com>
2015-04-17 14:09:53 +0200

    [ContractChecker] more readable tests

--- src/test/groovy/contracto/ContractCheckerSpec.groovy
+++ src/test/groovy/contracto/ContractCheckerSpec.groovy
@@ -10,27 +10,31 @@ class ContractCheckerSpec extends Specification {
 
     def "Should return true when object match body"() {
         expect:
-        checkClassMatchItem(MyData, newItem(object, 'id', string, null))
+        checkClassMatchItem(MyData, newItem(object, 'id', string))
     }
 
-    def "Should return false when object doesn't match body"() {
+    def "Should return false when class field has wrong name"() {
         expect:
-        !checkClassMatchItem(MyData, newItem(type, embeddedItemName, embeddedItemType, embeddedembedded))
-        where:
-        type   | embeddedItemName | embeddedItemType | embeddedembedded
-        object | 'name'           | string           | null
-        object | 'id'             | number           | null
-        object | 'id'             | object           | [newItem(string, null, null, null)]
+        !checkClassMatchItem(MyData, newItem(object, 'wrongName', string))
     }
 
-    private static Item newItem(Type type, String embeddedItemName, Type embeddedItemType, List<Item> embeddedembedded) {
+    def "Should return false when class field has wrong type"() {
+        expect:
+        !checkClassMatchItem(MyData, newItem(object, 'id', number))
+    }
+
+    def "Should return false when expected simple type"() {
+        expect:
+        !checkClassMatchItem(MyData, new Item(type: string))
+    }
+
+    private static Item newItem(Type type, String embeddedItemName, Type embeddedItemType) {
         return new Item(
                 type: type,
                 embedded: [
                         new Item(
                                 name: embeddedItemName,
                                 type: embeddedItemType,
-                                embedded: embeddedembedded,
                         ),
                 ],
         )
