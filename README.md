f1a333a1ed002d5e0fe4e8cb4d823449a939c0fb
Maciej Górski <maciek.gorski@gmail.com>
2015-04-18 01:04:19 +0200

    Run http server. Don't use network in tests.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![Build Status](https://travis-ci.org/contracto-lab/contracto-retrofit.svg?branch=master)](https://travis-ci.org/contracto-lab/contracto-retrofit)
 
 Contracto is a framework for API end-to-end testing. This repo contains bindings for Retrofit - popular Android (and Java) REST client library.
+
+To run tests, first start http server in the root directory of this project:
+
+    python -m SimpleHTTPServer 13579
+
+and then execute:
+
+    ./gradlew test
--- src/test/groovy/contracto/api/ContractoServiceSpec.groovy
+++ src/test/groovy/contracto/api/ContractoServiceSpec.groovy
@@ -9,8 +9,8 @@ class ContractoServiceSpec extends Specification {
     def "should return same contract"() {
         given:
         List<Contract> contracts = new ContractoService().downloadContracts([
-                "https://raw.githubusercontent.com/contracto-lab/contracto-retrofit/master/contracts/my_data.con.json",
-                "https://raw.githubusercontent.com/contracto-lab/contracto-retrofit/master/contracts/other_data.con.json",
+                "http://localhost:13579/contracts/my_data.con.json",
+                "http://localhost:13579/contracts/other_data.con.json",
         ])
         expect:
         contracts == [ContractStub.contract(), ContractStub.otherContract()]
