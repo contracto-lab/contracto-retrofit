5a2044fad86a9f9f744b96e9f3970a67e79c1b12
Ordon <ordon1313@gmail.com>
2015-04-17 20:35:15 +0200

    Runtime magic

--- src/main/groovy/contracto/ContractoClassType.groovy
+++ src/main/groovy/contracto/ContractoClassType.groovy
@@ -1,6 +1,10 @@
 package contracto
 
 import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.FieldNode
+import org.codehaus.groovy.ast.GenericsType
+import org.codehaus.groovy.ast.MethodNode
 
 import java.lang.reflect.Field
 import java.lang.reflect.Method
@@ -8,21 +12,42 @@ import java.lang.reflect.Method
 @CompileStatic
 class ContractoClassType {
     static ContractoClassType fromMethod(Method method, Class inClass) {
-        if(method.returnType instanceof List){
-            //TODO make funny logic here
-            throw new UnsupportedOperationException('TODO make some List magic')
-        }else{
-            return new ContractoClassType(type: method.returnType)
-        }
+        Closure<ClassNode> classNode = ContractoClassType.&classNodeFromMethod.curry(method, inClass)
+        return fromClass(method.returnType, classNode)
     }
+
+    private static ClassNode classNodeFromMethod(Method method, Class inClass) {
+        ClassNode classNode = new ClassNode(inClass)
+        MethodNode methodNode = classNode.methods.find { it.name == method.name }
+        return methodNode.returnType
+    }
+
     static ContractoClassType fromField(Field field, Class inClass) {
-        if(field.type instanceof List){
-            //TODO make funny logic here
-            throw new UnsupportedOperationException('TODO make some List magic')
-        }else{
-            return new ContractoClassType(type: field.type)
+        Closure<ClassNode> classNode = ContractoClassType.&classNodeFromField.curry(field, inClass)
+        return fromClass(field.type, classNode)
+    }
+
+    private static ClassNode classNodeFromField(Field field, Class inClass) {
+        ClassNode classNode = new ClassNode(inClass)
+        FieldNode fieldNode = classNode.fields.find { it.name == field.name }
+        return fieldNode.type
+    }
+
+    private static ContractoClassType fromClass(Class<?> type, Closure<ClassNode> typeNode) {
+        if (type == List) {
+            ClassNode classNode = typeNode.call()
+            Class genericType = extractGenericType(classNode)
+            return new ContractoClassType(type: type, genericType: genericType)
+        } else {
+            return new ContractoClassType(type: type)
         }
     }
+
+    private static Class extractGenericType(ClassNode returnType) {
+        GenericsType genericsType = returnType.genericsTypes[0]
+        return genericsType.type.getTypeClass()
+    }
+
     Class type
     Class genericType
 
