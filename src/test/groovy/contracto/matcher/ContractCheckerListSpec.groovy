4bce45a4502e0e603f1c8b00dafe00bbf193e96c
Maciej Górski <maciek.gorski@gmail.com>
2015-04-19 12:58:22 +0200

    Get List<String> type directly.

--- src/test/groovy/contracto/matcher/ContractCheckerListSpec.groovy
+++ src/test/groovy/contracto/matcher/ContractCheckerListSpec.groovy
@@ -1,12 +1,14 @@
 package contracto.matcher
 
+import com.google.gson.reflect.TypeToken
 import contracto.handler.DefaultContractsWithMatchHandler
 import contracto.model.ContractStub
 import contracto.model.contract.Item
 import contracto.model.reflect.ContractoClassType
 import spock.lang.Specification
 
-import static contracto.model.contract.Type.*
+import static contracto.model.contract.Type.array
+import static contracto.model.contract.Type.number
 
 class ContractCheckerListSpec extends Specification {
 
@@ -43,11 +45,8 @@ class ContractCheckerListSpec extends Specification {
         ))
     }
 
-    List<String> stringList
-
     private ContractoClassType stringListType() {
-        def field = ContractCheckerListSpec.getDeclaredField('stringList')
-        return ContractoClassType.fromField(field)
+        return new ContractoClassType(type: new TypeToken<List<String>>() {}.type)
     }
 
     private boolean checkClassMatchItem(ContractoClassType classType, Item item) {
